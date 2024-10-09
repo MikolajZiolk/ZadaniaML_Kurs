db3c8d1fd7d55ffb608c11bdb13e88951c49f3f2
Mikolaj Ziolkowski <mikolajziolk@gmail.com>
2024-10-09 21:26:03 +0200

    Dostosowanie danych

--- Zadania/Cwiczenie1/Cwiczenie_1.R
+++ Zadania/Cwiczenie1/Cwiczenie_1.R
@@ -1,1 +1,16 @@
 #zadanie1
+library(tidyverse)
+library(GGally)
+
+colnames(airquality) <- tolower(colnames(airquality)) #zmiana nazwy kolumn na małe litery
+
+air <-
+  airquality |>
+  as_tibble() |> #format wizualizacji tibble
+  na.omit() |> #usuniecie wierszy z brakami danych
+  select(-day) |> #usuniecie kolumny day
+  mutate(month = factor(month)) #konwersja na typ faktor
+
+View(air) # wyświetlenie tabeli
+
+glimpse(air) 
