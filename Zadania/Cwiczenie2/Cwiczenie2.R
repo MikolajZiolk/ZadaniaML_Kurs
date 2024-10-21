3285b10286b1ddb79821b33193ebcc7191b2624d
Mikolaj Ziolkowski <mikolajziolk@gmail.com>
2024-10-21 23:11:55 +0200

    usuniecie pustych danych

--- Zadania/Cwiczenie2/Cwiczenie2.R
+++ Zadania/Cwiczenie2/Cwiczenie2.R
@@ -6,5 +6,11 @@ library(openair)
 tidymodels_prefer()
 
 
-air <- mydata |> selectByDate(year = 2004) 
+air <- mydata |> selectByDate(year = 2002) 
 air |> skim()
+str(air)
+
+air <- air |> na.omit() #pominięcie pusty danych
+
+
+
