633ec79e6aa0f58d48b184cfafbff90efd46677e
Mikolaj Ziolkowski <mikolajziolk@gmail.com>
2024-10-22 08:56:05 +0200

    wykresy

--- Zadania/Cwiczenie2/Cwiczenie2.R
+++ Zadania/Cwiczenie2/Cwiczenie2.R
@@ -1,7 +1,8 @@
 #Cwiczenie2
 library(tidymodels) 
 library(skimr) 
-library(GGally) 
+library(GGally)
+library(ggpubr)
 library(openair) 
 tidymodels_prefer()
 
@@ -12,5 +13,26 @@ str(air)
 
 air <- air |> na.omit() #pominięcie pusty danych
 
+set.seed(222)
+air[sample(1:nrow(air), size = 300, replace = F),] |> 
+  select(nox, no2) |> 
+  ggpairs()
 
+
+# wykres regresji liniowej, do sprawdzenia danych 
+set.seed(222)
+air[sample(1:nrow(air), size = 300, replace = F),] |> 
+  select(nox, no2) |> 
+  ggplot(aes(nox, no2)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = T, formula = y ~ x) + 
+  stat_cor(label.x = 10, label.y = 80) + 
+  stat_regline_equation(label.x = 10, label.y = 82) +
+  theme_bw()
+
+
+air |>    
+  ggplot(aes(date, o3)) +     
+  geom_line() +     
+  theme_bw()
 
