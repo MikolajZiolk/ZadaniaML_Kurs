5e481da7d5981ddd94d37a5cc91ff2b2ffd43e2a
Mikolaj Ziolkowski <mikolajziolk@gmail.com>
2024-10-26 12:06:36 +0200

    krzywa ROC

--- Zadania/Cwiczenie2/Cwiczenie2.R
+++ Zadania/Cwiczenie2/Cwiczenie2.R
@@ -75,22 +75,45 @@ lr_mod <-
   logistic_reg() |> 
   set_engine("glm")
 
-logi_work <- 
+oz_work <- 
   workflow() |> 
   add_model(lr_mod) |> 
   add_recipe(oz_rec)
 
-logi_work
+oz_work
 
-logi_fit <-
-  logi_work |> 
+oz_fit <-
+  oz_work |> 
   fit(data =train_data)
 
 oz_rec |> summary()
 
-logi_fit |> 
+oz_fit |> 
   extract_fit_parsnip() |> 
-  extract_recipe() |> 
-    tidy()
+  tidy() |> 
+  mutate(coef_stars = signif_stars(p.value)) #istotność
+
+predict(oz_fit, test_data)
+predict(oz_fit, test_data, type="prob")
+
+pred_test <- 
+  augment(oz_fit, test_data) |>
+  select(-ws,
+         -wd,
+         -nox,
+         -o3,
+         -nox,
+         -no2,
+         -pm10,
+         -pm25,
+         -so2,
+         -co,
+         -date)
+pred_test
+
+pred_test |> 
+  roc_curve(truth = ozone, .pred_Niskie) |> 
+  autoplot()
+
 
 
