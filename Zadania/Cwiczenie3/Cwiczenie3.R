10be45ee3fe659ec6a7ecf6d4baea854ab8a8a17
Mikolaj Ziolkowski <mikolajziolk@gmail.com>
2024-11-01 23:12:51 +0100

    utworzenie pliku R

---
+++ Zadania/Cwiczenie3/Cwiczenie3.R
@@ -0,0 +1,121 @@
+#Cwiczenie3 na podstawie 2
+library(tidymodels) 
+library(skimr) 
+library(GGally)
+library(ggpubr)
+library(openair) 
+tidymodels_prefer()
+
+
+air <- mydata |> selectByDate(year = 2002) |> na.omit()
+air |> skim()
+
+
+set.seed(222)
+air[sample(1:nrow(air), size = 300, replace = F),] |> 
+  select(nox, no2) |> 
+  ggpairs()
+
+
+# wykres regresji liniowej, do sprawdzenia danych 
+set.seed(222)
+air[sample(1:nrow(air), size = 300, replace = F),] |> 
+  select(nox, no2) |> 
+  ggplot(aes(nox, no2)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = T, formula = y ~ x) + 
+  stat_cor(label.x = 10, label.y = 80) + 
+  stat_regline_equation(label.x = 10, label.y = 82) +
+  theme_bw()
+
+air |>    
+  ggplot(aes(date, o3)) +     
+  geom_line() +     
+  theme_bw()
+
+# Podgląd zakresu wartości ozonu
+air |> 
+  pull(o3) |> 
+  range()  
+
+# Tworzenie zmiennej jakościowej 'ozone' 
+air <-
+  air |>
+  mutate(ozone = cut(
+    o3,
+    breaks = c(-0.1, 10, 53),
+    labels = c("Niskie", "Wysokie")
+  ))
+
+air |> count(ozone)
+
+#podział na zbiór trenigowy i testowy
+set.seed(222)
+data_split <-initial_split(air, prop = 0.75, strata = ozone)
+train_data <- training(data_split)
+test_data <- testing(data_split)
+
+# Tworzenie receptury
+oz_rec <- 
+  recipe(ozone ~ ., data = train_data) |>
+  update_role(o3, wd, date, pm10, pm25, so2, co, no2, new_role = "ID") |> 
+  step_BoxCox(ws, nox, no2) |>
+  step_date(date, features = c("month")) |> #kolumna jakościowa
+  step_time(date, features = c("hour")) |> 
+  step_mutate(date_hour = as.factor(date_hour)) |>  
+  step_dummy(all_nominal_predictors()) |>  # Tworzenie zmiennych fikcyjnych
+  step_zv() #usunie kolumny z danych, gdy dane zestawu szkoleniowego mają pojedynczą wartość,
+
+oz_rec |> summary()
+
+oz_rec |>  ##??????
+  prep()
+
+lr_mod <-
+  logistic_reg() |> 
+  set_engine("glm")
+
+oz_work <- 
+  workflow() |> 
+  add_model(lr_mod) |> 
+  add_recipe(oz_rec)
+
+oz_work
+
+oz_fit <-
+  oz_work |> 
+  fit(data =train_data)
+
+oz_rec |> summary()
+
+oz_fit |> 
+  extract_fit_parsnip() |> 
+  tidy() |> 
+  mutate(coef_stars = signif_stars(p.value)) #istotność
+
+predict(oz_fit, test_data)
+predict(oz_fit, test_data, type="prob")
+
+pred_test <- 
+  augment(oz_fit, test_data) |>
+  select(-ws,
+         -wd,
+         -nox,
+         -o3,
+         -nox,
+         -no2,
+         -pm10,
+         -pm25,
+         -so2,
+         -co,
+         -date)
+pred_test
+
+pred_test |> 
+  roc_curve(truth = ozone, .pred_Niskie) |> 
+  autoplot()
+
+pred_test |> 
+  roc_auc(truth = ozone, .pred_Niskie)
+
+
