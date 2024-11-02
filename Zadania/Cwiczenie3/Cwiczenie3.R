e6d0c3efe6e800f7e280cde9fd543a1cb202d85c
Mikolaj Ziolkowski <mikolajziolk@gmail.com>
2024-11-02 12:28:46 +0100

    wszystkie metryki

--- Zadania/Cwiczenie3/Cwiczenie3.R
+++ Zadania/Cwiczenie3/Cwiczenie3.R
@@ -144,16 +144,32 @@ rf_workflow <-
   add_model(rf_mod) |> 
   add_recipe(oz_rec)
 
-#tune 
+#tune rf
 set.seed(456)
 rf_fit_rs <-
   rf_workflow |> 
   fit_resamples(cv_folds)
 
+rf_fit_bootstrap <- 
+  rf_workflow |>
+  fit_resamples(bootstrap_folds) 
+
+rf_fit_r5 <- 
+  rf_workflow |> 
+  fit_resamples(cv_folds_r5)
+
 # tune lr
 lr_fit_rs <-
   oz_work |> 
   fit_resamples(cv_folds)
+
+lr_fit_bootstrap <- 
+  oz_work |>
+  fit_resamples(bootstrap_folds) 
+
+lr_fit_r5 <- 
+  oz_work |> 
+  fit_resamples(cv_folds_r5)
 
 #rozdzxielenie wartosci
 rf_fit_rs |> 
@@ -170,19 +186,47 @@ metrics_without_resample <- bind_rows(
 ) |>
   knitr::kable(digits = 3)
 
-#metryki z resample
+#metryki
 #lr
 metrics_lr_cv_folds <- lr_fit_rs |> 
   collect_metrics() |> 
   filter(.metric == "accuracy" | .metric == "roc_auc") |>
   mutate(.approach = "lr_cv_folds")
+
+metrics_lr_vcv_r5 <- lr_fit_r5|> 
+  collect_metrics() |> 
+  filter(.metric == "accuracy" | .metric == "roc_auc") |>
+  mutate(.approach = "lr_cv_r5")
+
+metrics_lr_bootstrap <- lr_fit_bootstrap|> 
+  collect_metrics() |> 
+  filter(.metric == "accuracy" | .metric == "roc_auc") |>
+  mutate(.approach = "lr_bootstrap")
+
 #rf
 metrics_rf_cv_folds <- rf_fit_rs|> 
   collect_metrics() |> 
   filter(.metric == "accuracy" | .metric == "roc_auc") |>
   mutate(.approach = "rf_cv_folds")
 
+metrics_rf_vcv_r5 <- rf_fit_r5|> 
+  collect_metrics() |> 
+  filter(.metric == "accuracy" | .metric == "roc_auc") |>
+  mutate(.approach = "rf_cv_r5")
+
+metrics_rf_bootstrap <- rf_fit_bootstrap|> 
+  collect_metrics() |> 
+  filter(.metric == "accuracy" | .metric == "roc_auc") |>
+  mutate(.approach = "rf_bootstrap")
+
+
 all_metrics <- bind_rows(
+  metrics_without_resample,
   metrics_lr_cv_folds,
-  metrics_rf_cv_folds
+  metrics_lr_vcv_r5,
+  metrics_lr_bootstrap,
+  metrics_rf_cv_folds,
+  metrics_rf_vcv_r5,
+  metrics_rf_bootstrap
 )
+
